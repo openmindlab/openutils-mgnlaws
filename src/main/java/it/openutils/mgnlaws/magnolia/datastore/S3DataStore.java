9e2650974729e8b4029c9caeeb56af97f3b23072
molaschi <manuel.molaschi@gmail.com>
2014-03-27 17:58:07 +0100

    remove old credentials from file

--- src/main/java/it/openutils/mgnlaws/magnolia/datastore/S3DataStore.java
+++ src/main/java/it/openutils/mgnlaws/magnolia/datastore/S3DataStore.java
@@ -16,9 +16,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-// Access Key Id: AKIAJZ42NZG2V4RVZVYQ
-// Secret Access Key: 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
 package it.openutils.mgnlaws.magnolia.datastore;
 
 import java.io.File;
